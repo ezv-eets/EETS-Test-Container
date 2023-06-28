df2a6bb6864f5ddf3eb56c083804a7c555134f42
Thomas Kayser <thomas.kayser@bit.admin.ch>
2023-06-28 14:32:58 +0200

    switch from rabbit to kafka

--- run-tc-integration-test.sh
+++ run-tc-integration-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose --file docker-compose.yml build --no-cache
-docker-compose --file docker-compose.yml --file .integration-test/docker-compose.test.yml run testcontainer-verification-tss
-docker-compose --file docker-compose.yml --file .integration-test/docker-compose.test.yml run testcontainer-verification-pis
+docker compose --file docker-compose.yml build --no-cache
+docker compose --file docker-compose.yml --file .integration-test/docker-compose.test.yml run testcontainer-verification-tss
+docker compose --file docker-compose.yml --file .integration-test/docker-compose.test.yml run testcontainer-verification-pis
